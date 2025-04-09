c346b2d42e1ad7752310cfea9b3e08301a4ff330
vishal2970 <v.ag7570@gmail.com>
2025-04-09 16:47:45 +0530

    Created page for testing in nextjs

---
+++ src/app/secondpage/page.tsx
@@ -0,0 +1,9 @@
+export default function secondPage(){
+    return (
+        <div className="second-page">
+            <div className="second-page__header">
+                <h1 className="text-center text-6xl mt-5">Second Page</h1>
+            </div>
+        </div>
+    )
+}
